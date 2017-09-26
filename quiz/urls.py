6ec6ceb2098903ea65b221c5b255a6b59f32438c
Dennis Tupas <denis0826@gmail.com>
2017-09-26 20:42:37 +0800

    quiz update

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
-from .import views
+from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^login/$', views.my_login, name='login'),
     url(r'^logout/$', views.my_logout, name='logout'),
+    url(r'^register/$', views.register, name='register'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,16 +1,52 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.views.generic import View
+from django.views import generic
 
 from .models import Quiz
 
 def index(request):
-    return render(request, 'index.html', {})
+    queryset = Quiz.published.all()
 
-class LoginView(ListView):
-    def get(self, request):
-        return HttpResponse('result')
+    context = {
+        'objects_quiz' : queryset,
+        'title': "Quiz Builder"
+    }
+    return render(request, 'index.html', context)
+
+
+def my_login(request):   
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return None
+
+    else:
+        return render(request, 'login.html', {})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
 
 def my_logout(request):
     try:
