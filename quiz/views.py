bf1680373d90ab53c63fad3823f41421f348f5eb
Dennis Tupas <denis0826@gmail.com>
2017-09-29 16:20:14 +0800

    quizbuild update

--- quiz/views.py
+++ quiz/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.views.generic import View
 from django.views import generic
 
-from .models import Quiz
+from .models import Quiz, Question
 
 def index(request):
     queryset = Quiz.objects.all()
@@ -55,6 +55,37 @@ def my_logout(request):
         pass
     return redirect('login')
 
-def quiz(request):
-    quiz = Quiz.objects.get(id=quiz_id)
-    return render(request, 'quiz.html', {'quiz': quiz})
+# def quiz(request, quiz_id):
+#     quiz = Quiz.objects.get(pk=quiz_id)
+
+#     context = {
+#         'quiz': quiz,
+#         'title': "Quiz"
+#     }
+    
+#     return render(request, 'quiz.html', context ) 
+
+# def quiz_list(request, quiz_id):    
+#     question = get_object_or_404(Quiz, pk=quiz_id)
+#     return render(request, 'quiz.html', {'question': question})
+
+class DetailView(generic.DetailView):
+    model = Quiz
+    template_name = 'quiz.html'
+
+
+def choose(request, quiz_id):
+    p = get_object_or_404(Quiz, pk=quiz_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
--- quizbuilder/urls.py
+++ quizbuilder/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('quiz.urls')),
+    url(r'quiz/^', include('quiz.urls'), namespace="quiz"),
 ]
